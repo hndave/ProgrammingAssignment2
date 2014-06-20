0ac2e2e0e6fd855a57fe7e3c3d20cbed6d939e4f
Hitesh Dave <hndave@yahoo.com>
2014-06-20 15:33:45 -0500

    Cleaned up some comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,12 @@
 # -  invMatrix - stores the inverse of the matrix X.
 # -  Function defines four methods for getters/setters for the two local variables X, invMatrix
 # -  a list of these four methods is returned to the caller.
+
 #Usage: a <- makeCacheMatrix(rep(1,6),2,3)
 # 	a$set(matrix(1:6, 2, 3)) , set a matrix in a
 #	m <- a$get(), returns the stored matrix
 #	im <- a$getInverse(), returns the stored inverse of the matrix
 #	a$setInverse(matrix), sets the matrix to the  inverse matrix variable
-
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 
 makeCacheMatrix <- function(x = matrix()) {
 	# Defaul constructor for this object sets the invMatrix to NULL, 
@@ -51,15 +46,17 @@ makeCacheMatrix <- function(x = matrix()) {
 #cacheSolve takes a special 'matrix' object X as an input parameter 
 #and sets the inverse of the matrix stored in X
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 
 		#Check if inverse already exists?
+		# if Yes, do nothing, just print a message that returning cached value`
 		if(!is.null( x$getInverse() )) {
 			# Do Nothing
-			message ("Inverse Already exits !")
+			message ("Inverse Already exits!, cached value returned")
 		} else {
-			#call the setInverse method and pass the result of the solve(x$get(), ...)
+			#invoke the setInverse method on 'x' and pass the result of the solve(x$get(), ...)
 			x$setInverse( solve(x$get(), ... ))
 		}
+
+		## Return a matrix that is the inverse of 'x'
 		x$getInverse()
 }
